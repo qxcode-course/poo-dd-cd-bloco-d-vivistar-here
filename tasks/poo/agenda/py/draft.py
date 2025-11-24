190d1d164519c956fd569845f048fcdbeeb8ccfd
Vitoria de Jesus <vitoriadejesusstar@alu.ufc.br>
2025-11-24 16:11:27 +0000

    inicio do fone

---
+++ tasks/poo/agenda/py/draft.py
@@ -0,0 +1,10 @@
+class Fone:
+    def __init__(self, id: str, number: str):
+        self.id = id
+        self.number = number
+
+    def getId(self):
+        return self.id
+
+    def getNumber(self):
+        return self.number
