3c105a3d224a6f8ae1d2095bb0ccd0231e433813
Vitoria de Jesus <vitoriadejesusstar@alu.ufc.br>
2025-11-26 23:04:00 +0000

    class fone

--- tasks/poo/agenda/py/draft.py
+++ tasks/poo/agenda/py/draft.py
@@ -8,3 +8,15 @@ class Fone:
 
     def getNumber(self):
         return self.number
+
+    def isValid(self) -> bool:
+        validos = "0123456789"
+        for c in self.number:
+            if c not in validos:
+                return False
+            return True
+
+    def __str__(self):
+        return f"{self.id}:{self.number}"
+
+    
